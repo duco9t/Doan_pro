293ddec7a933391dda9afef22684566ac0842bcd
duytran1508 <ngocduyxx1508@gmail.com>
2024-11-14 13:40:10 +0700

    vnpay

--- src/models/OrderModel.js
+++ src/models/OrderModel.js
@@ -1,47 +1,39 @@
 const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema(
-  {
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true
-    },
-    cartId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Cart",
-      required: true
-    },
-    products: [
-      {
-        productId: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Product",
-          required: true
-        },
-        quantity: { type: Number, required: true }
-      }
-    ],
-    name: { type: String, required: true },
-    phone: { type: String, required: true },
-    email: { type: String, required: true },
-    shippingAddress: {
-      address: { type: String, required: true },
-      city: { type: String },
-      country: { type: String }
-    },
-    totalPrice: { type: Number, required: true },
-    shippingFee: { type: Number, default: 0 },
-    VATorder: { type: Number, default: 0 },
-    orderTotal: { type: Number, required: true },
-    status: {
-      type: String,
-      enum: ["Pending", "Shipped", "Delivered", "Cancelled"],
-      default: "Pending"
-    }
+const orderSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
   },
-  { timestamps: true }
-);
+  cartId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Cart",
+    required: true
+  },
+  products: [
+    {
+      productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+      quantity: { type: Number, required: true }
+    }
+  ],
+  shippingAddress: {
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    postalCode: { type: String, required: true },
+    country: { type: String, required: true }
+  },
+  totalPrice: { type: Number, required: true },
+  shippingFee: { type: Number, default: 0 },
+  orderTotal: { type: Number, required: true },
+  status: {
+    type: String,
+    enum: ["Pending", "Shipped", "Delivered", "Cancelled"],
+    default: "Pending"
+  }
+}, { timestamps: true });
+
+
 
 const Order = mongoose.model("Order", orderSchema);
 module.exports = Order;
